ad623101ab09da4a519434a6b60ec0fcfe9a1012
Vitaliy <morfeezz.cs@gmail.com>
2023-10-20 23:40:25 +0500

    added azimuth calc

--- video_detect_realtime_Ruslan.py
+++ video_detect_realtime_Ruslan.py
@@ -7,12 +7,15 @@ import math
 import m3u8
 import urllib.request
 import matplotlib.path as mplPath
+from deep_sort_realtime.deepsort_tracker import DeepSort # 14.09.23
 
 import tqdm
+
 
 from vidgear.gears import WriteGear
 
 from scipy.interpolate import interp1d
+from geographiclib.geodesic import Geodesic
 
 CURR_DIR = os.path.dirname(os.path.realpath(__file__))
 
@@ -136,205 +139,10 @@ frames_persec = 25
 hours_perframe = 1 / 60 / 60 / frames_persec
 default_distance = 25#euclid metric
 
-from geographiclib.geodesic import Geodesic
-
-# def get_bearing(lat1, long1, lat2, long2 ):
-#     brng = Geodesic.WGS84.Inverse(lat1, long1, lat2, long2)['azi1']
-#     return brng
 
 def get_bearing(lat1, long1, lat2, long2 ):
     brng = Geodesic.WGS84.Inverse(round(lat1, 6), round(long1, 6), round(lat2, 6), round(long2, 6))['azi1']
     return brng
-
-def calculate_initial_compass_bearing(pointA, pointB):
-    """
-    Calculates the bearing between two points.
-    The formulae used is the following:
-        θ = atan2(sin(Δlong).cos(lat2),
-                  cos(lat1).sin(lat2) − sin(lat1).cos(lat2).cos(Δlong))
-    :Parameters:
-      - `pointA: The tuple representing the latitude/longitude for the
-        first point. Latitude and longitude must be in decimal degrees
-      - `pointB: The tuple representing the latitude/longitude for the
-        second point. Latitude and longitude must be in decimal degrees
-    :Returns:
-      The bearing in degrees
-    :Returns Type:
-      float
-    """
-    if (type(pointA) != tuple) or (type(pointB) != tuple):
-        raise TypeError("Only tuples are supported as arguments")
-
-    lat1 = math.radians(pointA[0])
-    lat2 = math.radians(pointB[0])
-
-    diffLong = math.radians(pointB[1] - pointA[1])
-
-    x = math.sin(diffLong) * math.cos(lat2)
-    y = math.cos(lat1) * math.sin(lat2) - (math.sin(lat1)
-            * math.cos(lat2) * math.cos(diffLong))
-
-    initial_bearing = math.atan2(x, y)
-
-    # Now we have the initial bearing but math.atan2 return values
-    # from -180° to + 180° which is not what we want for a compass bearing
-    # The solution is to normalize the initial bearing as shown below
-    initial_bearing = math.degrees(initial_bearing)
-    compass_bearing = (initial_bearing + 360) % 360
-
-    return compass_bearing
-
-def calc_bearing(llat1, llong1, llat2, llong2):
-    #pi - число pi, rad - радиус сферы (Земли)
-    rad = 6372795
-    
-    #координаты двух точек
-    # llat1 = 77.1539
-    # llong1 = -120.398
-    
-    # llat2 = 77.1804
-    # llong2 = 129.55
-    
-    #в радианах
-    lat1 = llat1*math.pi/180.
-    lat2 = llat2*math.pi/180.
-    long1 = llong1*math.pi/180.
-    long2 = llong2*math.pi/180.
-    
-    #косинусы и синусы широт и разницы долгот
-    cl1 = math.cos(lat1)
-    cl2 = math.cos(lat2)
-    sl1 = math.sin(lat1)
-    sl2 = math.sin(lat2)
-    delta = long2 - long1
-    cdelta = math.cos(delta)
-    sdelta = math.sin(delta)
-    
-    #вычисления длины большого круга
-    y = math.sqrt(math.pow(cl2*sdelta,2)+math.pow(cl1*sl2-sl1*cl2*cdelta,2))
-    x = sl1*sl2+cl1*cl2*cdelta
-    ad = math.atan2(y,x)
-    dist = ad*rad
-    
-    #вычисление начального азимута
-    x = (cl1*sl2) - (sl1*cl2*cdelta)
-    y = sdelta*cl2
-    z = math.degrees(math.atan(-y/x))
-    
-    if (x < 0):
-        z = z+180.
-    
-    z2 = (z+180.) % 360. - 180.
-    z2 = - math.radians(z2)
-    anglerad2 = z2 - ((2*math.pi)*math.floor((z2/(2*math.pi))) )
-    angledeg = (anglerad2*180.)/math.pi
-    
-    return angledeg
-
-def get_bearing1(lat1,lon1,lat2,lon2):
-    dLon = lon2 - lon1
-    y = math.sin(dLon) * math.cos(lat2)
-    x = math.cos(lat1)*math.sin(lat2) - math.sin(lat1)*math.cos(lat2)*math.cos(dLon)
-    brng = np.rad2deg(math.atan2(y, x))
-    if brng < 0: brng+= 180
-    return brng
-
-def get_bearing2(lat1, long1, lat2, long2):
-    # Convert latitude and longitude to radians
-    lat1 = math.radians(lat1)
-    long1 = math.radians(long1)
-    lat2 = math.radians(lat2)
-    long2 = math.radians(long2)
-    
-    # Calculate the bearing
-    bearing = math.atan2(
-        math.sin(long2 - long1) * math.cos(lat2),
-        math.cos(lat1) * math.sin(lat2) - math.sin(lat1) * math.cos(lat2) * math.cos(long2 - long1)
-    )
-    
-    # Convert the bearing to degrees
-    bearing = math.degrees(bearing)
-    
-    # Make sure the bearing is positive
-    bearing = (bearing + 360) % 360
-    
-    return bearing
-    
-def get_bearing3(lat1, long1, lat2, long2):
-    dLon = (long2 - long1)
-    x = math.cos(math.radians(lat2)) * math.sin(math.radians(dLon))
-    y = math.cos(math.radians(lat1)) * math.sin(math.radians(lat2)) - math.sin(math.radians(lat1)) * math.cos(math.radians(lat2)) * math.cos(math.radians(dLon))
-    brng = np.arctan2(x,y)
-    brng = np.degrees(brng)
-
-    return brng    
-
-def distance_bearing(homeLatitude, homeLongitude, destinationLatitude, destinationLongitude):
-    R = 6371e3
-    """
-    Simple function which returns the distance and bearing between two geographic location
-
-    Inputs:
-        1.  homeLatitude            -   Latitude of home location
-        2.  homeLongitude           -   Longitude of home location
-        3.  destinationLatitude     -   Latitude of Destination
-        4.  destinationLongitude    -   Longitude of Destination
-
-    Outputs:
-        1. [Distance, Bearing]      -   Distance (in metres) and Bearing angle (in degrees)
-                                        between home and destination
-
-    Source:
-        https://github.com/TechnicalVillager/distance-bearing-calculation
-    """
-
-    rlat1   =   homeLatitude * (math.pi/180) 
-    rlat2   =   destinationLatitude * (math.pi/180) 
-    rlon1   =   homeLongitude * (math.pi/180) 
-    rlon2   =   destinationLongitude * (math.pi/180) 
-    dlat    =   (destinationLatitude - homeLatitude) * (math.pi/180)
-    dlon    =   (destinationLongitude - homeLongitude) * (math.pi/180)
-
-    # Haversine formula to find distance
-    a = (math.sin(dlat/2) * math.sin(dlat/2)) + (math.cos(rlat1) * math.cos(rlat2) * (math.sin(dlon/2) * math.sin(dlon/2)))
-    c = 2 * math.atan2(math.sqrt(a), math.sqrt(1-a))
-
-    # Distance in metres
-    distance = R * c
-
-    # Formula for bearing
-    y = math.sin(rlon2 - rlon1) * math.cos(rlat2)
-    x = math.cos(rlat1) * math.sin(rlat2) - math.sin(rlat1) * math.cos(rlat2) * math.cos(rlon2 - rlon1)
-    
-    # Bearing in radians
-    bearing = math.atan2(y, x)
-    bearingDegrees = bearing * (180/math.pi)
-    out = [distance, bearingDegrees]
-
-    return out
-
-def get_bearing4(lat1, lon1, lat2, lon2):
-    lon1, lat1, lon2, lat2 = map(math.radians, [lon1, lat1, lon2, lat2])
-
-    dlon = lon2 - lon1
-    dlat = lat2 - lat1
-    a = math.sin(dlat/2)**2 + math.cos(lat1) * math.cos(lat2) * math.sin(dlon/2)**2
-    c = 2 * math.atan2(math.sqrt(a), math.sqrt(1-a))
-    Base = 6371 * c
-
-
-    Bearing = math.atan2(math.cos(lat1)*math.sin(lat2)-math.sin(lat1)*math.cos(lat2)*math.cos(lon2-lon1), math.sin(lon2-lon1)*math.cos(lat2))
-
-    Bearing = math.degrees(Bearing)
-    
-    return Bearing
-
-def calc_angle(ax, ay, bx, by):
-    ma = math.sqrt(ax * ax + ay * ay)
-    mb = math.sqrt(bx * bx + by * by)
-    sc = ax * bx + ay * by
-    res = np.degrees(math.acos(sc / ma / mb))
-    return res
 
 def interp_azimuth(df):
     for i in df['id'].unique():
@@ -356,20 +164,6 @@ def interp_azimuth(df):
         df[['azimuth']][df['id'] == i] = pd.DataFrame(to_pd)
         return df
 
-import pyproj
-
-# def get_bearing(lat1, long1, lat2, long2):
-#     geodesic = pyproj.Geod(ellps='clrk66')
-#     fwd_azimuth,back_azimuth,distance = geodesic.inv(long1, lat1, long2, lat2)
-#     return fwd_azimuth
-
-
-
-
-import time # 14.09.23
-from deep_sort_realtime.deepsort_tracker import DeepSort # 14.09.23
-import sort # 14.09.23
-import cvzone
 
 object_tracker = DeepSort(max_age=5,
                           n_init=3,
@@ -385,10 +179,6 @@ object_tracker = DeepSort(max_age=5,
                           embedder_wts=None,
                           polygon=False,
                           today=None) # 14.09.23
-
-# ------------------------SORT--------------------------------
-# object_tracker = sort.Sort(max_age=50, min_hits=2, iou_threshold=0.5)
-
 
 def main():
     count = 0
@@ -434,8 +224,6 @@ def main():
                 count += 1
                 
                 results = model(frame)
-                # ------------------------SORT--------------------------------
-                # detections = np.empty((0, 5))
                 detections = []
                 
                 df = results.pandas().xyxy[0]
@@ -459,21 +247,14 @@ def main():
 
                         gps_coord = getRealcoords((cx, cy), True)
                         
-                        # image = cv2.rectangle(frame, (x[0], y[0]), (x[1], y[1]), color_dict[tup[5]], 8)# object rectangle
-                        # image = cv2.rectangle(image, (dist_x[0], dist_y[0]), (dist_x[1], dist_y[1]), (255,255,255), 1)# euclid metric rectangle
-                        # image = cv2.circle(image, (cx, cy), 5, (0, 0, 255), -1)# center of object
                         image = cv2.putText(image, tup[6], (x[0], y[0] - 5), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2) # label of object
                         image = cv2.putText(image, str(gps_coord[0]) +" "+ str(gps_coord[1]), (x[0] - 30, y[0] + 15), cv2.FONT_HERSHEY_SIMPLEX, 0.6, (0, 255, 0), 2)# GPS label of object
                         center_points_cur_frame.append(((cx, cy), tup[6]))
                         
                         currentArray = np.array([x[0], y[0], x[1], y[1], tup[4]])
                         
-                        # ------------------------SORT--------------------------------
-                        # detections = np.vstack((detections, currentArray))
                         detections.append(([x[0], y[0], abs(x[1]-x[0]), abs(y[1]-y[0])], tup[4], tup[6]))
                         
-                # ------------------------SORT--------------------------------
-                # resultsTracker = object_tracker.update(detections)
                 resultsTracker = object_tracker.update_tracks(detections, frame=frame)
                 nameCount = 0
                 for track in resultsTracker:
@@ -485,50 +266,13 @@ def main():
                     gps_crd = getRealcoords((ltrb[0], ltrb[1]), True)
                     az_points.append((gps_crd, track.get_det_class()))
                     
-                    
-                    # print(ltrb)
-                    # print(track.get_det_class)
                     bbox = ltrb
-                    a = track
-                    image = cv2.rectangle(frame, (int(bbox[0]), int(bbox[1])), (int(bbox[2]), int(bbox[3])), (255,255,255), 2)
-                    # image = cv2.putText(image, str(track_id), (int(bbox[0]), int(bbox[1]) - 5), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 1) # label of object
-                    
+                    image = cv2.rectangle(frame, (int(bbox[0]), int(bbox[1])), (int(bbox[2]), int(bbox[3])), (255,255,255), 2)                    
                     image = cv2.putText(image, track.get_det_class(), (int(bbox[0]), int(bbox[1]) - 5), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2) # label of object
-                    # image = cv2.putText(image, str(gps_coord[0]) +" "+ str(gps_coord[1]), (x[0] - 30, y[0] + 15), cv2.FONT_HERSHEY_SIMPLEX, 0.6, (0, 255, 0), 2)# GPS label of object
                     nameCount += 1
                 
-                # ------------------------SORT--------------------------------
-                # for result in resultsTracker: 
-                #     x1, y1, x2, y2, id = result
-                #     x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2)
-                    
-                #     cx = int(x1 - (x1 - x2) / 2)
-                #     cy = int(y1 + (y2 - y1) / 2)
-                    
-                #     gps_coord = getRealcoords((cx, cy), True)
-                    
-                #     # print(result)
-                #     image = cv2.rectangle(frame, (x1, y1), (x2, y2), (255,255,255), 2)
-                #     image = cv2.circle(image, (cx, cy), 5, (0, 0, 255), -1)# center of object
-                #     image = cv2.putText(image, str(id), (x1, y1 - 5), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 1) # label of object
-                #     image = cv2.putText(image, str(gps_coord[0]) +" "+ str(gps_coord[1]), (x1 - 30, y1 + 15), cv2.FONT_HERSHEY_SIMPLEX, 0.6, (0, 255, 0), 2)# GPS label of object
-                #     # center_points_cur_frame.append(((cx, cy), nameList))
-                
-                # for pt, cls in az_points:
-                #     for pt2, cls2 in az_points:
 
-                #         az_pt, az_pt2 = getRealcoords(pt, True), getRealcoords(pt2, True) # 21.09
-                        
-                #         if distance < default_distance:
-                #             azimuth = get_bearing(az_pt[0], az_pt[1], 
-                #                         az_pt2[0], az_pt2[1]) # 21.09
-                #             print(az_pt[0], az_pt[1], az_pt2[0], az_pt2[1], azimuth)
-                
-                
-                
-                    
-                
-                
+
                 sub_img = image[0:437, 0:470]
                 white_rect = np.ones(sub_img.shape, dtype=np.uint8) * 0
                 res = cv2.addWeighted(sub_img, 0.4, white_rect, 0.5, 1.0)
@@ -581,7 +325,6 @@ def main():
                                             az_pt2[0], az_pt2[1]) # 21.09
                                 speed_t = (real_distance / 10000) / hours_perframe
                                 tracking_objects[object_id] = pt, speed_t, cls, azimuth
-                                # print(az_pt[0], az_pt[1], az_pt2[0], az_pt2[1], azimuth) # 21.09
                                 object_exists = True
                                 if (pt, cls) in center_points_cur_frame:
                                     center_points_cur_frame.remove((pt, cls))
@@ -606,32 +349,10 @@ def main():
                     gps_coord = getRealcoords((pt[0], pt[1]), True)#GPS
                     gps.append(getRealcoords((pt[0], pt[1]), True))
                     if az_count > 0:
-                        # fwd_azimuth,back_azimuth,distance = geodesic.inv(float(gps[az_count][0]), float(gps[az_count][1]), 
-                        #                                                  float(gps[az_count - 1][0]), float(gps[az_count - 1][1]))
-                        
-                        # azimuth = get_bearing(float(gps[az_count][0]), float(gps[az_count][1]), 
-                        #                       float(gps[az_count - 1][0]), float(gps[az_count - 1][1]))
-                        # ax = float(gps[az_count][0]) - float(gps[az_count - 1][0])
-                        # ay = float(gps[az_count][1]) - float(gps[az_count - 1][1])
-                        # north_x, north_y = 54.725498 - 54.725357, 55.940651 - 55.940654
-                        # angle = calc_angle(ax, ay, north_x, north_y)
-                        
-                        # azimuth = get_bearing(float(gps[az_count - 1][0]), float(gps[az_count - 1][1]), 
-                        #                      float(gps[az_count][0]), float(gps[az_count][1]))
-                        
-                        # azimuth = calc_bearing(float(gps[az_count][0]), float(gps[az_count][1]), 
-                        #                       float(gps[az_count - 1][0]), float(gps[az_count - 1][1]))
-                        
-                        # azimuth = calculate_initial_compass_bearing((float(gps[az_count][0]), float(gps[az_count][1])), (float(gps[az_count - 1][0]), float(gps[az_count - 1][1])))
-                        # image = cv2.putText(image, f"{str(object_id)} s: {'%.2f'%speed} km/h  az: {azimuth}", (pt[0] - 12, pt[1] - 7), cv2.FONT_HERSHEY_SIMPLEX, 1, (0,0,255), 2)
-                        
                         image = cv2.putText(image, f"{str(object_id)} az: {azimuth}", (pt[0] - 12, pt[1] - 7), cv2.FONT_HERSHEY_SIMPLEX, 1, (0,0,255), 2)
-                        
-                        # image = cv2.putText(image, f"{str(object_id)} az: {fwd_azimuth}", (pt[0] - 12, pt[1] - 7), cv2.FONT_HERSHEY_SIMPLEX, 1, (0,0,255), 2)
+                    image = cv2.putText(image, f"{str(object_id)} s: {'%.2f'%speed} km/h  az: {azimuth}", (pt[0] - 12, pt[1] - 7), cv2.FONT_HERSHEY_SIMPLEX, 1, (0,0,255), 2) 
                     az_count += 1
                     
-                    
-
                     lane_n = None
                     for k, traffic_lane in enumerate(traffic_lanes_path):
                         if traffic_lane.contains_point((pt[0], pt[1])):
@@ -653,8 +374,6 @@ def main():
 
                 center_points_prev_frame = center_points_cur_frame.copy()
                 
-                # cv2.imshow('frame', frame)
-                # cv2.waitKey(1)
                 
                 if cv2.waitKey(1) == ord('q'):
                     cap.release()
